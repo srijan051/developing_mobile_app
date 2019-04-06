d2b99d7c07e83c8ed802a2b70411b056cf942c28
Srijan Duwal <srijanduwal@yahoo.com>
2019-04-06 23:05:40 +0545

    FragmentExample2 Complete

---
+++ week6/FragmentExample2/app/src/main/java/com/example/fragmentexample2/SecondActivity.java
@@ -0,0 +1,82 @@
+package com.example.fragmentexample2;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+    private Button mButton;
+    private boolean isFragmentDisplayed = false;
+    static final String STATE_FRAGMENT = "state_of_fragment";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        mButton = findViewById(R.id.open_button);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!isFragmentDisplayed){
+                    displayFragment();
+                }
+                else {
+                    closeFragment();
+                }
+            }
+        });
+
+        if(savedInstanceState !=null){
+            isFragmentDisplayed = savedInstanceState.getBoolean(STATE_FRAGMENT);
+            if(isFragmentDisplayed){
+                //if the fragment is displayed, change the button to close
+                mButton.setText(R.string.close);
+            }
+        }
+    }
+    public void onSaveInstanceState(Bundle saveInstanceState) {
+        saveInstanceState.putBoolean(STATE_FRAGMENT,isFragmentDisplayed);
+        super.onSaveInstanceState(saveInstanceState);
+    }
+
+    public void displayFragment(){
+        SimpleFragment simpleFragment = SimpleFragment.newInstance();
+        //Get fragmentManager and start a transaction
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        //Add the SimpleFragment
+        fragmentTransaction.add(R.id.fragment_container,simpleFragment).addToBackStack(null).commit();
+        //Update the button text
+        mButton.setText(R.string.close);
+        //set boolean flag to indicate fragment is open
+        isFragmentDisplayed = true;
+    }
+
+    public void closeFragment(){
+        //Get the fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        //Check to see if the fragment is already showing
+        SimpleFragment simpleFragment = (SimpleFragment) fragmentManager.findFragmentById(R.id.fragment_container);
+        if(simpleFragment != null){
+            //Create and commit hte transaction to remove the fragment
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.remove(simpleFragment).commit();
+        }
+        //update the button text
+        mButton.setText(R.string.open);
+        //set boolean value to indicate fragment is close
+        isFragmentDisplayed = false;
+    }
+
+    public void btnPrevious(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
--- week6/FragmentExample2/app/src/main/java/com/example/fragmentexample2/SimpleFragment.java
+++ week6/FragmentExample2/app/src/main/java/com/example/fragmentexample2/SimpleFragment.java
@@ -1,10 +1,8 @@
-package com.example.android.fragmentexample1;
+package com.example.fragmentexample2;
 
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,11 +10,14 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 
 
+/**
+ * A simple {@link Fragment} subclass.
+ */
 public class SimpleFragment extends Fragment {
 
+    private static final int YES = 0;
+    private static final int NO = 1;
 
-    private static final int yes = 0;
-    private static final int no = 1;
 
     public SimpleFragment() {
         // Required empty public constructor
@@ -27,38 +28,37 @@ public class SimpleFragment extends Fragment {
     }
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
 
-        final View rootView = inflater.inflate(R.layout.fragment_simple, container, false);
-        final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
+        //Inflate the layout for this fragment
+        final View rootView = inflater.inflate(R.layout.fragment_simple,container,false);
+        final RadioGroup radioGroup = rootView.findViewById(R.id.radio_grp);
 
-
+        //Set the radioGroup onCheckedChanged Listner
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 View radioButton = radioGroup.findViewById(checkedId);
                 int index = radioGroup.indexOfChild(radioButton);
-                TextView textView =  rootView.findViewById(R.id.fragment_header);
+                TextView textView = rootView.findViewById(R.id.fragment_header);
                 switch (index){
-                    case yes:
+                    case YES:
                         textView.setText(R.string.yes_message);
                         break;
-
-                    case no:
+                    case NO:
                         textView.setText(R.string.no_message);
                         break;
-                     default:
-                         break;
+                    default:
+                        break;
+
                 }
             }
         });
 
+        //Return the view for the fragment's UI
         return rootView;
-
     }
 
 }
